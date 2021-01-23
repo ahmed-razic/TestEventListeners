ac499724c9689503ed25f2c9abd919d548798c37
ahmed-razic <ahmed.razic@gmail.com>
2021-01-23 18:07:14 +0100

    added OnClickListener

--- app/src/main/java/com/example/android/practice/MainActivity.java
+++ app/src/main/java/com/example/android/practice/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void numbers(View view) {
 
-        Intent openNumbers = new Intent(this, NumbersActivity.class);
-        startActivity(openNumbers);
+        TextView toNumbers = findViewById(R.id.toNumbers);
+        toNumbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openNumbers = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(openNumbers);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/android/practice/NumbersActivity.java
+++ app/src/main/java/com/example/android/practice/NumbersActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class NumbersActivity extends AppCompatActivity {
 
